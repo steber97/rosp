464be2c610c19fddf438714a5ff184748f800d9a
Stefano Huber <stefano.huber@gmail.com>
2026-01-30 19:14:28 +0100

    Remove assert for PSDness in eig_lb

--- eig.py
+++ eig.py
@@ -6,5 +6,5 @@ def eig_lb(M: np.ndarray, *args) -> float:
     """
     Compute the smallest true eigenvalue of M.
     """
-    assert psd(M)
+    # assert psd(M)
     return np.min([x.real for x in np.linalg.eig(M)[0]])
