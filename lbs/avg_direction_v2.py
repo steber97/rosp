43725783d5ba3a7c77d4a7661f76c74594cd7e76
Stefano Huber <stefano.huber@gmail.com>
2026-04-14 11:45:31 +0200

    avg_direction if there is an element on the diagonal <1, then all entries are divided by it

--- lbs/avg_direction_v2.py
+++ lbs/avg_direction_v2.py
@@ -1,7 +1,9 @@
 import numpy as np
+from pprint import pprint
 from typing import Tuple
+from time import time
 
-from utils import EPS, create_rand_dd_plus_ros
+from utils import EPS, create_rand_psd_matrix
 from lbs.gershgorin import gershgorin_lb
 from piecewise_linear_maximize import maximize_x
 from lbs.greedy_pm_shift import max_direction_lb
@@ -12,7 +14,7 @@ def avg_direction_v2_lb(M: np.ndarray, *args) -> float:
             as the sum of rank-repetitions vectors.
     """
     n = len(M)
-    repetitions = int(args[0]) if len(args) > 0 else 2  # Use as default 2
+    repetitions = int(args[0]) if len(args) > 0 else 1  # Use as default 2
     M_copy = M.copy()
     for rank_k_approx in range(repetitions):
         dd_value = [(
@@ -20,36 +22,45 @@ def avg_direction_v2_lb(M: np.ndarray, *args) -> float:
             M_copy[i,i] - np.sum(np.abs(np.concatenate((M_copy[i, :i], [] if i == n-1 else M_copy[i, i+1:]))))
             ) for i in range(n)]
         dd_value = sorted(dd_value, key=lambda x: x[1])
-        rep = int(np.sum([x[1] < EPS for x in dd_value]))
+        # pprint(dd_value)
+        rep = int(np.sum([x[1] < -EPS for x in dd_value]))
         directions = []
         if rep > 0:
             for k in range(rep):
                 directions.append(np.ones(n))
                 i, dd_val = dd_value[k]
+                directions[-1][i] = min(1, M_copy[i,i])
                 for j in range(n):
                     if j != i:
-                        directions[-1][j] = M_copy[i,j]
+                        directions[-1][j] = M_copy[i,j] / (min(1, M_copy[i,i]))
                 directions[-1] /= np.sqrt(directions[-1] @ directions[-1])
                 assert abs(directions[-1]@directions[-1] - 1) < EPS
                 if k > 0:
                     if directions[-1] @ directions[0] < EPS:
                         directions[-1] = -directions[-1]
-            
+            # pprint(directions)
             tot = np.sum([dd_value[k][1] for k in range(rep)])
+            # print(tot)
             direction = np.sum([directions[k] * dd_value[k][1] / tot for k in range(rep)], axis=0)
+            # print(direction)
             direction /= np.sqrt(direction @ direction)
-            
+            # print(direction)
             # print(direction, direction @ direction)
             x = maximize_x(M_copy, np.outer(direction, direction))
+            # print(x)
             # x = 1
             M_copy -= x * np.outer(direction, direction)
-
+            # print(M_copy)
+    
     return gershgorin_lb(M_copy)
 
 if __name__=="__main__":
-    n = 10
-    M = create_rand_dd_plus_ros(n, 0.1)
-    print(M)
-    lb = avg_direction_v2_lb(M)
-    print(lb)
+    n = 500
+    M = create_rand_psd_matrix(n)
+    # print(M)
+    print(gershgorin_lb(M))
+    time_start = time()
+    print(avg_direction_v2_lb(M))
+    print("time total:", time() - time_start)
+
 
